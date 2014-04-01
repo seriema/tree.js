785541b4010918e45d75b305ad3420f9528a7c15
John-Philip Johansson <seriema@gmail.com>
2014-04-01 15:50:17 -0700

    Add basic interface and failing tests.

--- test/tree_test.js
+++ test/tree_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var treejs = require('../lib/tree.js');
+var tree = require('../lib/tree.js');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -24,13 +24,51 @@ var treejs = require('../lib/tree.js');
 
 exports.treejs = {
   setUp: function(done) {
-    // setup here
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(3);
+
     done();
   },
-  'no args': function(test) {
+
+  'find root': function(test) {
     test.expect(1);
-    // tests here
-    test.equal(treejs.awesome(), 'awesome', 'should be awesome.');
+
+    test.equal(tree.lookup(2), true, 'should find node.');
+    test.done();
+  },
+
+  'find left node': function(test) {
+    test.expect(1);
+
+    test.equal(tree.lookup(1), true, 'should find node.');
+    test.done();
+  },
+
+  'find right node': function(test) {
+    test.expect(1);
+
+    test.equal(tree.lookup(3), true, 'should find node.');
+    test.done();
+  },
+
+
+  'dont find missing node': function(test) {
+    test.expect(1);
+
+    test.equal(tree.lookup(4), false, 'should not find node.');
+    test.done();
+  },
+
+  'print simple tree': function(test) {
+    test.expect(1);
+
+    var expected = '' +
+      '  2\n' +
+      ' / \\\n' +
+      '1   3\n';
+
+    test.equal(tree.print(), expected, 'should print tree.');
     test.done();
   }
 };
